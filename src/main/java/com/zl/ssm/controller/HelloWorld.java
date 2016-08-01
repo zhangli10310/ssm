cc065bc461512001a2b612e8dec4b01cf147e613
zhangli <1031041235@qq.com>
2016-08-01 23:48:53 +0800

    nothing

--- src/main/java/com/zl/ssm/controller/HelloWorld.java
+++ src/main/java/com/zl/ssm/controller/HelloWorld.java
@@ -2,6 +2,7 @@ package com.zl.ssm.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * Created by zhangli on 16/8/1 17:19.</br>
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HelloWorld {
 
     @RequestMapping(value = "/hello")
+    @ResponseBody
     public String index() {
         return "index";
     }
